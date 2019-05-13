0199f4a4ef7ba5875bd9397f0e7eee1daecefdbd
scottfones <scott.fones@gmail.com>
2019-05-13 16:41:03 -0400

    added tmdb_key.py to hold api key. added initial imports to webmify and set up main()

---
+++ webmify.py
@@ -0,0 +1,13 @@
+import re
+import tmdbsimple as tmdbimport
+import subprocess
+from pathlib import Path
+from collections import Counter
+from optparse import OptionParser
+
+
+def main():
+    print('Hello world!')
+
+if __name__ == '__main__':
+    main()
